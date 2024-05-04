774d1174d907680ed4e5a1dd9fd96ad2b9e9df60
manasjhamj <jhamanas939@gmail.com>
2024-05-04 21:42:19 +0530

    change png to webp for faster loading

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react'
+import start from '../../assets/start.webp'
 
-function Navbar({onHoverChange}) {
+function Navbar({ onHoverChange }) {
 
-    const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState(new Date());
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -16,34 +17,34 @@ function Navbar({onHoverChange}) {
 
   return (
     <nav>
-        <button className="start-col" 
+      <button className="start-col"
         onMouseEnter={() => onHoverChange(true)}
         onMouseLeave={() => onHoverChange(false)}
-        >
-            <img className="start-img" src="http://opi.wiki/pic?w=6&f=108883&e=2&o=703" alt="" />
-            <p className="start">Start</p>
-        </button>
+      >
+        <img className="start-img" src={start} alt="" />
+        <p className="start">Start</p>
+      </button>
 
-        <div className="nav">
-            <div className="tabs">
-                <a className="tab-inner" href="./index.html">
-                    <img className="tab-icons" src="https://www.manasjha.me/windows-xp/images/icons/facebook.png" alt="" />
-                    <h4>Read About me</h4>
-                </a>
-                <a className="tab-inner" href="./index.html">
-                    <img className="tab-icons" src="https://www.manasjha.me/windows-xp/images/icons/tweetdeck.png" alt="" />
-                    <h4>See my Projects</h4>
-                </a>
-                <a className="tab-inner" href="./index.html">
-                    <img className="tab-icons" src="https://www.manasjha.me/windows-xp/images/icons/outlook.png" alt="" />
-                    <h4>Contact Me</h4>
-                </a>
-            </div>
-            <div className="time">
-                <p><img className='icons' src='https://www.manasjha.me/windows-xp/images/icons/computer.png'/></p>
-                <p>{date.toLocaleTimeString()}</p>
-            </div>
+      <div className="nav">
+        <div className="tabs">
+          <a className="tab-inner" href="./index.html">
+            <img className="tab-icons" src="https://www.manasjha.me/windows-xp/images/icons/facebook.png" alt="" />
+            <h4>Read About me</h4>
+          </a>
+          <a className="tab-inner" href="./index.html">
+            <img className="tab-icons" src="https://www.manasjha.me/windows-xp/images/icons/tweetdeck.png" alt="" />
+            <h4>See my Projects</h4>
+          </a>
+          <a className="tab-inner" href="./index.html">
+            <img className="tab-icons" src="https://www.manasjha.me/windows-xp/images/icons/outlook.png" alt="" />
+            <h4>Contact Me</h4>
+          </a>
         </div>
+        <div className="time">
+          <p><img className='icons' src='https://www.manasjha.me/windows-xp/images/icons/computer.png' /></p>
+          <p>{date.toLocaleTimeString()}</p>
+        </div>
+      </div>
     </nav>
   )
 }
